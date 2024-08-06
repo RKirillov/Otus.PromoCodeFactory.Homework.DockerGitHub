5ddf24089745b3e50dedc62e7a5c52935aae8c7e
Roman Kirillov <RKirillov@live.ru>
2024-08-06 16:33:01 +0300

    fix: ase changed to Postgre

--- src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
+++ src/Otus.Teaching.PromoCodeFactory.WebHost/Startup.cs
@@ -13,11 +13,19 @@ using Otus.Teaching.PromoCodeFactory.Core.Domain.Administration;
 using Otus.Teaching.PromoCodeFactory.DataAccess;
 using Otus.Teaching.PromoCodeFactory.DataAccess.Data;
 using Otus.Teaching.PromoCodeFactory.DataAccess.Repositories;
+using Microsoft.Extensions.Configuration;
 
 namespace Otus.Teaching.PromoCodeFactory.WebHost
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -28,7 +36,7 @@ namespace Otus.Teaching.PromoCodeFactory.WebHost
             services.AddScoped<IDbInitializer, EfDbInitializer>();
             services.AddDbContext<DataContext>(x =>
             {
-                x.UseSqlite("Filename=PromoCodeFactoryDb.sqlite");
+                x.UseNpgsql(Configuration.GetConnectionString("PromoCodeFactoryDb"));
                 x.UseLazyLoadingProxies();
             });
 
