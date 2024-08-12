a7639781a9c2a537b48dea8695252d868826c245
Роман Кириллов <RKirillov@live.ru>
2024-08-12 15:31:51 +0300

    fix: added tests, refactored dockerfile

---
+++ src/Otus.Teaching.PromoCodeFactory.UnitTests/WebHost/SetPartnerPromoCodeLimitAsyncTests.cs
@@ -0,0 +1,20 @@
+﻿
+using Xunit;
+
+namespace Otus.Teaching.PromoCodeFactory.UnitTests.WebHost
+{
+    public class SetPartnerPromoCodeLimitAsyncTests
+    {
+        public SetPartnerPromoCodeLimitAsyncTests()
+        {
+
+        }
+
+        [Fact]
+        public  void EmptyTest()
+        {
+            // Assert
+            Assert.Equal("Hello world!", "Hello world!");
+        }
+    }
+}
